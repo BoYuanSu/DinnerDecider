38e825ee4c7e74cc3cb8b346d54595596410a2e5
su <mayday15221@gmail.com>
2019-04-20 15:03:23 +0800

    Finish Learning

--- app/src/main/java/com/takosu/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/takosu/dinnerdecider/MainActivity.kt
@@ -2,11 +2,31 @@ package com.takosu.dinnerdecider
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("MOS", "McDonalds", "爵克貓", "丼之家", "小屋", "飽飽", "銀魚", "餃子呦", "阮家", "米香", "民欣", "肉麻麵","")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        txtSelectFood.text = "Burger King"
+
+        btnDecide.setOnClickListener {
+            println("You clicked me!")
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            txtSelectFood.text = foodList[randomFood]
+        }
+        btnAddFood.setOnClickListener {
+            val newFood = txtAddFood.text.toString()
+            if (newFood != "") {
+                foodList.add(newFood)
+                txtAddFood.text.clear()
+            }
+        }
     }
 }
